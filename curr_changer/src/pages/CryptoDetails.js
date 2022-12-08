a821d491c65e8266f9fa8279d4fe51485bba39a6
iluxa2003 <ilya.yakov.com@gmail.com>
2022-12-08 15:54:40 +0200

    sorting on detailed crypto

--- curr_changer/src/pages/CryptoDetails.js
+++ curr_changer/src/pages/CryptoDetails.js
@@ -5,20 +5,55 @@ import Table from "react-bootstrap/Table";
 import "./CryptoDetails.css";
 const CryptoDetails = () => {
   const { id } = useParams();
-  const [cryptoHistory, setCryptoHistory] = useState([0, 0]);
+  const [cryptoHistory, setCryptoHistory] = useState([
+    { time: 0, priceUsd: 0 },
+  ]);
+  // const [sortedCryptoHistory, setSortedCryptoHistory] = useState([]);
+  // const [index, setIndex] = useState(5);
+  const [period, setPeriod] = useState("h6");
   useEffect(() => {
-    fetch("https://api.coincap.io/v2/assets/" + id + "/history?interval=h1")
+    fetch(
+      "https://api.coincap.io/v2/assets/" + id + "/history?interval=" + period
+    )
       .then((response) => response.json())
       .then((response) => {
         setCryptoHistory(response.data);
       });
-  }, [id]);
+  }, [id, period]);
+  // useEffect(() => {
+  //   setAmount();
+  // }, [cryptoHistory, index]);
+  const timePeriodSort = (event) => {
+    setPeriod(parseInt(event.target.value));
+  };
+  // const setAmount = (event) => {
+  //   // setIndex(event.target.value);
+  //   setSortedCryptoHistory([cryptoHistory[0]]);
+  //   for (let i = 1; i < 10; i++) {
+  //     if (cryptoHistory.length != 1) {
+  //       setSortedCryptoHistory([...sortedCryptoHistory, cryptoHistory[i]]);
+  //     }
+  //   }
+  // };
   return (
     <>
       <Header />
       <main>
         <section className="crypto-details__info">
           <h3>{"Histori of changes --- " + id}</h3>
+          <span>History for </span>
+          <select value={period} onChange={timePeriodSort}>
+            <option value="m1">every minute</option>
+            <option value="m5">every 5 minutes</option>
+            <option value="m15">every 15 minutes</option>
+            <option value="m30">every 30 minutes</option>
+            <option value="h1">every hour</option>
+            <option value="h2">every 2 hours</option>
+            <option value="h6">every 6 hours</option>
+            <option value="h12">every 12 hours</option>
+            <option value="d1">every day</option>
+          </select>
+          {/* <input type="number" onInput={setAmount} /> */}
           <Table striped bordered hover variant="dark">
             <thead>
               <tr>
@@ -27,24 +62,21 @@ const CryptoDetails = () => {
               </tr>
             </thead>
             <tbody>
-              {/* <tr>
-                {cryptoHistory.map((item) => {
-                  return <td>{Date(item.time)}</td>;
-                })}
-              </tr>
-              <tr>
-                {cryptoHistory.map((item) => {
-                  return <td>{item.priceUsd + "USD"}</td>;
-                })}
-              </tr> */}
               {cryptoHistory.map((item) => {
                 return (
-                  <tr>
+                  <tr key={Math.random()}>
                     <td>{new Date(item.time).toUTCString()}</td>
                     <td>{parseFloat(item.priceUsd).toFixed(2) + "USD"}</td>
                   </tr>
                 );
               })}
+              {/* {
+              for(let i = 0; i<=100;i++){
+                  <tr key={Math.random()}>
+                    <td>{new Date(item.time).toUTCString()}</td>
+                    <td>{parseFloat(item.priceUsd).toFixed(2) + "USD"}</td>
+                  </tr>
+              }} */}
             </tbody>
           </Table>
         </section>
